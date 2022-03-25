dc55e60b49930480e48f7803d17a5c26390066fb
Jenda <jan.reindl@inforis.cz>
2022-03-25 14:39:55 +0100

    Přidejte soubory projektu.

---
+++ AnotherWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using ControlzEx.Theming;
+using MahApps.Metro;
+using MahApps.Metro.Controls;
+
+namespace FlyoutThemingBug
+{
+    /// <summary>
+    /// Interakční logika pro AnotherWindow.xaml
+    /// </summary>
+    public partial class AnotherWindow : MetroWindow
+    {
+        public AnotherWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MetroWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            //sets a nice chartreause colour for good measure
+            ThemeManager.Current.ChangeTheme(this, RuntimeThemeGenerator.Current.GenerateRuntimeTheme("Light", (Color)ColorConverter.ConvertFromString("#7FFF00")));
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ControlzEx.Theming;
+using MahApps.Metro;
+using MahApps.Metro.Controls;
+
+namespace FlyoutThemingBug
+{
+    /// <summary>
+    /// Interakční logika pro MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MetroWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            //Sets a theme upon loading the window
+            ThemeManager.Current.ChangeTheme(Application.Current, RuntimeThemeGenerator.Current.GenerateRuntimeTheme("Dark", (Color)ColorConverter.ConvertFromString("#83003C")));
+        }
+        private void NewWindowBtn_Click(object sender, RoutedEventArgs e)
+        {
+            AnotherWindow window = new AnotherWindow();
+            window.Show();
+        }
+
+        private void FlyoutBtn_Click(object sender, RoutedEventArgs e)
+        {
+            //This is an easy way to remedy the bug
+            //MyFlyout.Theme = FlyoutTheme.Inverse;
+            //MyFlyout.Theme = FlyoutTheme.Adapt;
+
+            MyFlyout.IsOpen = MyFlyout.IsOpen != true;
+        }
+
+        
+    }
+}
